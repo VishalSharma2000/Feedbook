a7ed33d106f8f7e7d703ae1f7855fd80f965925a
vishal <vishaljhsharma@gmail.com>
2021-05-30 19:16:27 +0530

    Implemented Database connectivity setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+/* Connecting with mongodb atlas cluster */
+require('../src/db/mongooseConnection');
+
+/* Middlewares */
+app.use(express.json());        // For parsing the body of any POST requests
+app.use(helmet());
+app.use(morgan("common"));
 
 app.get('/', (req, res) => {
   res.send('Feedbook app is working');
---
+++ server/src/db/mongooseConnection.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = process.env.MONGO_URL;
+
+// Handle the initial connection error
+const makeConnection = async () => {
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }, console.log('Successfully Connected with Mongodb database'))
+  } catch (error) {
+    handleConnectionError(error);
+  }
+};
+
+makeConnection();
+
+/* 
+  Handle any errors emerging after initial connection is established properly. 
+  Since, this is basically handling the error received from mongodb we need to handle the initial
+  error manually - as done above using try-catch
+*/
+mongoose.connection.on('error', (error) => {
+  console.log(`Error from MongoDb: ${error}`);
+});
+
+const handleConnectionError = (error) => {
+  console.log('Fail in connection with mongodb' + error);
+
+  setTimeout(() => {
+    console.log('Will try to make connection again after 5 seconsds')
+  }, 5000);
+};
