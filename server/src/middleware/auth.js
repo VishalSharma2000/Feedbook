5c89a3d57998de723607649dcc2f1db003b0efe0
vishal <vishaljhsharma@gmail.com>
2021-07-04 00:41:09 +0530

    added error handling while authenticating user

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -5,18 +5,26 @@ const { JWT_VERIFY_KEY } = process.env;
 const User = require('../db/models/User');
 
 const authenticationUserToken = async (req, res, next) => {
+  /* Format of authorization header => Bearer authToken */
   const [authType, authToken] = req.headers['authorization'].split(' ');
-  const decoded = jwt.verify(authToken, JWT_VERIFY_KEY);
+  let decoded;
+  try {
+    decoded = jwt.verify(authToken, JWT_VERIFY_KEY);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(401).json('Authentication Failed');
+  }
 
   let user = undefined;
   try {
     user = await User.findOne({ _id: decoded._id, tokens: authToken });
   } catch (error) {
     console.log('Cannot Retrive User while token autherization');
-    throw Error('Authorization Failed');
+    return res.status(501).json('Authorication Failed');
   }
 
-  if (!user) throw new Error('Authorization Failed');
+  if (!user)
+    return res.status(401).json('Authentication Failed');
 
   req.user = user;
   req.token = authToken;
