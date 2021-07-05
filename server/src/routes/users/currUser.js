e0ce30abefb5e7edff19d4153707eeba5aaeee5b
vishal <vishaljhsharma@gmail.com>
2021-07-05 19:03:17 +0530

    Shift the follow and unfollow routes from currUser to user file
    All the routes in the user file are related to the currently logged user. In
    Future the route path will be changed. But follow and unfollow is the user actions
    not the personal user actions.
    
    Hence, to bring the routes in proper place shift the follow and unfollow routes
    to users file.
    
    Since, this is testing phase there is no side effects anywhere.

--- server/src/routes/users/currUser.js
+++ server/src/routes/users/currUser.js
@@ -4,6 +4,7 @@ const User = require('../../db/models/User');
 /* Update User Profile */
 router.patch('/update', async (req, res) => {
   const newUserDetails = req.body;
+  /* If the user is not currently logged user then don't update the details */
   if (newUserDetails._id != req.user._id) {
     return res.status(401).json('Authentication Failed');
   }
@@ -54,65 +55,6 @@ router.delete('/delete', async (req, res) => {
   } catch (error) {
     return res.status(501).json(failureJSON(error.message));
   }
-});
-
-/* Follow user */
-router.post('/follow', async (req, res) => {
-  const { userId, userToFollow } = req.body;
-
-  /* Check if User Exist */
-  const [currUserDetails] = await isUserExist(userId);
-  const [toFollowUserDetails] = await isUserExist(userToFollow);
-
-  if (!currUserDetails || !toFollowUserDetails) {
-    return res.status(401).json(failureJSON('User does not exist'));
-  }
-
-  /* If userToFollow is already following the user */
-  if (currUserDetails.following.includes(toFollowUserDetails._id)) {
-    return res.status(400).json(failureJSON('Already following the user'));
-  }
-
-  currUserDetails.following.push(toFollowUserDetails);
-  toFollowUserDetails.followers.push(currUserDetails);
-
-  try {
-    await currUserDetails.save();
-    await toFollowUserDetails.save();
-  } catch (error) {
-    return res.status(501).json(failureJSON(error));
-  }
-
-  res.json(currUserDetails);
-});
-
-/* UnFollow user */
-router.post('/unfollow', async (req, res) => {
-  const { userId, userToUnFollow } = req.body;
-
-  /* Check if User Exist */
-  const [currUserDetails] = await isUserExist(userId);
-  const [toUnFollowUserDetails] = await isUserExist(userToUnFollow);
-
-  if (!currUserDetails || !toUnFollowUserDetails) {
-    return res.status(401).json(failureJSON('User does not exist'));
-  }
-
-  /* If userToFollow is already following the user */
-  if (!currUserDetails.following.includes(toUnFollowUserDetails._id)) {
-    return res.status(400).json(failureJSON('You have already unfollowed the user'));
-  }
-
-  currUserDetails.following.slice(currUserDetails.following.indexOf(toUnFollowUserDetails), 1);
-  toUnFollowUserDetails.followers.pop(currUserDetails);
-
-  try {
-    await currUserDetails.save();
-  } catch (error) {
-    return res.status(501).json(failureJSON(error));
-  }
-
-  res.json(currUserDetails);
 });
 
 module.exports = router;
--- server/src/routes/users/users.js
+++ server/src/routes/users/users.js
@@ -41,5 +41,63 @@ router.get('/:id', async (req, res) => {
 });
 
 
+/* Follow user */
+router.post('/follow', async (req, res) => {
+  const { userId, userToFollow } = req.body;
+
+  /* Check if User Exist */
+  const [currUserDetails] = await isUserExist(userId);
+  const [toFollowUserDetails] = await isUserExist(userToFollow);
+
+  if (!currUserDetails || !toFollowUserDetails) {
+    return res.status(401).json(failureJSON('User does not exist'));
+  }
+
+  /* If userToFollow is already following the user */
+  if (currUserDetails.following.includes(toFollowUserDetails._id)) {
+    return res.status(400).json(failureJSON('Already following the user'));
+  }
+
+  currUserDetails.following.push(toFollowUserDetails);
+  toFollowUserDetails.followers.push(currUserDetails);
+
+  try {
+    await currUserDetails.save();
+    await toFollowUserDetails.save();
+  } catch (error) {
+    return res.status(501).json(failureJSON(error));
+  }
+
+  res.json(currUserDetails);
+});
+
+/* UnFollow user */
+router.post('/unfollow', async (req, res) => {
+  const { userId, userToUnFollow } = req.body;
+
+  /* Check if User Exist */
+  const [currUserDetails] = await isUserExist(userId);
+  const [toUnFollowUserDetails] = await isUserExist(userToUnFollow);
+
+  if (!currUserDetails || !toUnFollowUserDetails) {
+    return res.status(401).json(failureJSON('User does not exist'));
+  }
+
+  /* If userToFollow is already following the user */
+  if (!currUserDetails.following.includes(toUnFollowUserDetails._id)) {
+    return res.status(400).json(failureJSON('You have already unfollowed the user'));
+  }
+
+  currUserDetails.following.slice(currUserDetails.following.indexOf(toUnFollowUserDetails), 1);
+  toUnFollowUserDetails.followers.pop(currUserDetails);
+
+  try {
+    await currUserDetails.save();
+  } catch (error) {
+    return res.status(501).json(failureJSON(error));
+  }
+
+  res.json(currUserDetails);
+});
 
 module.exports = router;
