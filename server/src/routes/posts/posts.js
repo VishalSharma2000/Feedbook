7ea7d8b81218e2e763a863d78def81ffb492e84e
vishal <vishaljhsharma@gmail.com>
2021-07-23 12:39:28 +0530

    Update get post and create post route
    Get Post:
    post variable was declared as const, so changed it to let to prevent
    error. Add log error message so that whenever an error occur it would get
    loggin in the terminal
    
    Create Post:
    Authenticated user before creating post by checking their id with currently
    logged in user.

--- server/src/routes/posts/posts.js
+++ server/src/routes/posts/posts.js
@@ -11,20 +11,29 @@ router.use(verifyUser);
 router.get('/:postId', async (req, res) => {
   const postId = req.params.postId;
 
-  const post = undefined;
+  let post = undefined;
   try {
     post = await Post.findById(postId);
   } catch (err) {
+    console.log("Error while fetching post", err);
     return res.status(500).json({ err });
   }
 
-  if (!post) return res.status(404).send('Post does not exist');
+  if (!post) return res.status(404).json({ msg: 'Post does not exist' });
 
   return res.json(post);
 });
 
 /* Create Post */
 router.post('/create', async (req, res) => {
+  /* Embed userId if not given from frontend AND prevent some other user to create post on behalf of current logged in user */
+  const userId = req.body.userId;
+  if (userId === undefined) {
+    req.body.userId = req.user._id;
+  } else if (userId != req.user._id) {
+    return res.status(401).json({ msg: "You don't have access to create post" });
+  }
+
   const newPost = new Post(req.body);
 
   try {
